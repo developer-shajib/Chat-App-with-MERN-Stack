5cf7b05e0133675ff28f3592007c4f99a7bde11d
developer-shajib <sunhailshajib1@gmail.com>
2023-11-13 22:46:49 +0600

    Base url change

--- server/config/cors.js
+++ server/config/cors.js
@@ -1,5 +1,5 @@
 // allowed origin
-const allowedOrigins = ['http://localhost:3000', 'http://localhost:5173'];
+const allowedOrigins = ['http://localhost:3000', 'http://localhost:5173', 'https://chat-app-with-mern-stack.vercel.app/login'];
 
 // cors options
 const corsOptions = {
